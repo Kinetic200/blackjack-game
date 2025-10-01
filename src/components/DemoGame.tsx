43f9169a251a5beda411a75d59523efd09459f6a
michaelyichizhang <mzha0162@student.monash.edu>
2025-10-01 17:12:47 +1000

    Fix: Add missing splitDoubled field in DemoGame newGame function

--- src/components/DemoGame.tsx
+++ src/components/DemoGame.tsx
@@ -218,7 +218,8 @@ export default function DemoGame() {
       splitHand: null,
       splitScore: 0,
       activeSplitHand: null,
-      splitResults: { first: null, second: null }
+      splitResults: { first: null, second: null },
+      splitDoubled: { first: false, second: false }
     })
   }
 
